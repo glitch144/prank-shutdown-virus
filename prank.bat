bdb84fcde409246e958adcfe8fc33094855b43a0
glitch144 <77656306+glitch144@users.noreply.github.com>
2021-02-04 10:25:55 +0300

    Add files via upload

---
+++ prank.bat
@@ -0,0 +1,20 @@
+@echo off
+color a
+echo Executing Attack
+timeout /t 2 /nobreak >nul
+echo:
+echo Processing 
+timeout /t 2 /nobreak >nul
+echo:
+echo Deleting system files...
+echo corrupting Windows...
+echo Wiping Hard Drive...
+echo Gathering IP...
+timeout /t 2 /nobreak >nul
+echo:
+%SystemRoot%\System32\tree.com
+timeout /t 1 /nobreak >nul
+ipconfig
+echo:
+echo Shutting Down
+shutdown.exe /h
